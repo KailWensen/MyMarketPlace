e2605181e94642a222d4d8010e7b26b64c619a0a
Aviral Srivastava <kailwensen@gmail.com>
2023-09-04 14:48:05 +0530

    homepage filter pt 1

---
+++ client/src/Components/Prices.js
@@ -0,0 +1,27 @@
+export const Prices = [
+  {
+    _id: 0,
+    name: "Rs.0 to 500",
+    array: [0, 500],
+  },
+  {
+    _id: 1,
+    name: "Rs.600 to 1000",
+    array: [600, 1000],
+  },
+  {
+    _id: 2,
+    name: "Rs.1000 to 3000",
+    array: [1000, 3000],
+  },
+  {
+    _id: 3,
+    name: "Rs.3000 to 5000",
+    array: [3000, 5000],
+  },
+  {
+    _id: 3,
+    name: "Rs. 5000 <",
+    array: [5001, 99999],
+  },
+];
--- client/src/Pages/Homepage.js
+++ client/src/Pages/Homepage.js
@@ -1,15 +1,90 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Layout from "../Components/Layout/Layout";
-import { useAuth } from "../context/auth";
-
+import axios from "axios";
+import { Checkbox } from "antd";
 const Homepage = () => {
-  const [auth, setAuth] = useAuth();
+  const [products, setProducts] = useState([]);
+  const [categories, setCategories] = useState([]);
+  const [checked, setChecked] = useState([]);
+
+  // get all categories
+  const getAllCategory = async () => {
+    try {
+      const { data } = await axios.get("/api/v1/category/get-category");
+      if (data?.success) {
+        setCategories(data?.category);
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  useEffect(() => {
+    getAllCategory();
+  }, []);
+
+  //get all products
+  const getAllProducts = async () => {
+    try {
+      const { data } = await axios.get("/api/v1/product/get-product");
+      setProducts(data.products);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  //filter by category
+  const handleFilter = (value, id) => {
+    let all = [...checked];
+    if (value) {
+      all.push(id);
+    } else {
+      all = all.filter((c) => c !== id);
+    }
+    setChecked(all);
+  };
+  useEffect(() => {
+    getAllProducts();
+  }, []);
   return (
     <Layout title={"Homepage - MyMarketPlace"}>
-      <div className="row">
-        <div className="col-md-3"></div>
+      <div className="row mt-3">
+        <div className="col-md-2">
+          <h4 className="text-center">Filter By Category:</h4>
+          <div className="d-flex flex-column m-2">
+            {categories?.map((c) => (
+              <Checkbox
+                key={c._id}
+                onChange={(e) => handleFilter(e.target.checked, c._id)}
+              >
+                {c.name}
+              </Checkbox>
+            ))}
+          </div>
+        </div>
+        {JSON.stringify(checked, null, 4)}
         <div className="col-md-9">
-          <h1 className="text-center"></h1>
+          <h1 className="text-center">All Products</h1>
+          <div className="d-flex flex-wrap">
+            {products?.map((p) => (
+              <div className="card m-2" style={{ width: "18rem" }}>
+                <img
+                  src={`/api/v1/product/product-image/${p._id}`}
+                  className="card-img-top"
+                  alt={p.name}
+                />
+                <div className="card-body">
+                  <h5 className="card-title">{p.name}</h5>
+                  <p className="card-text">{p.description}</p>
+                  <button href="#" class="btn btn-primary ms-1">
+                    Details
+                  </button>
+                  <button href="#" class="btn btn-secondary ms-1">
+                    Add to Cart
+                  </button>
+                </div>
+              </div>
+            ))}
+          </div>
         </div>
       </div>
     </Layout>
