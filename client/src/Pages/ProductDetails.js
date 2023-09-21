78e5912005909e374a7f3bd945292389e7ab5009
Aviral Srivastava <kailwensen@gmail.com>
2023-09-21 17:05:18 +0530

    CategoriesDropdown

--- client/src/Pages/ProductDetails.js
+++ client/src/Pages/ProductDetails.js
@@ -25,9 +25,24 @@ const ProductDetails = () => {
 
   return (
     <Layout>
-      <div className="row container">
-        <div className="col-md-5">Image</div>
-        <div className="col-md-5">Details</div>
+      <div className="row container mt-2">
+        <div className="col-md-6">
+          <img
+            src={`/api/v1/product/product-image/${product._id}`}
+            className="card-img-top"
+            alt={product.name}
+            height="500"
+            width="350"
+          />
+        </div>
+        <div className="col-md-6 ">
+          <h1 className="text-center">Product Detalis</h1>
+          <h6>Name: {product.name}</h6>
+          <h6>Description: {product.description}</h6>
+          <h6>Price: {product.price}</h6>
+          {/* <h6>Category: {product.category.name}</h6> */}
+          <button class="btn btn-secondary ms-1">ADD TO CART</button>
+        </div>
       </div>
       <div className="col-md-5">Similar Products</div>
     </Layout>
